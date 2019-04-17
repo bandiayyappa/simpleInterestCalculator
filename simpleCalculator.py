a29edc41a4734accdb634a5ba94827dedd89d608
Ayyappa Bandi <47629149+bandiayyappa@users.noreply.github.com>
2019-04-17 12:28:58 +0530

    Update simpleCalculator.py

--- simpleCalculator.py
+++ simpleCalculator.py
@@ -1,3 +1,26 @@
-s=[int(input('enter number')) for item in range(3) ]
-s.sort(reverse=True)
-print(s[0])
+#  interest calculator
+
+while True:
+    p = input('enter Principal amount ')
+    if not p.isdigit():
+        print('value Error:\n\t\t Amount should be numbers only ')
+        continue
+    break
+
+while True:
+    t = input('enter number of years ')
+    if not t.isdigit():
+        print('value Error:\n\t\t Years should be numbers only ')
+        continue
+    break
+
+while True:
+    r = input('enter rate of interest in Rs for every 100Rs ')
+    if not r.isdigit():
+        print('value Error:\n\t\t Rate should be numbers only')
+        continue
+    break
+p = int(p)
+t = int(t)
+r = int(r)
+print('Interest = ',((p*t*r)/100))
